b04f807e0312419295f4b1d59546e230b9436506
tranphuc2026abc-prog <tranphuc2026abc@gmail.com>
2025-11-21 08:01:46 +0700

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -5,7 +5,7 @@ import glob
 import time
 from pypdf import PdfReader
 # --- CÁC THƯ VIỆN RAG CHUẨN (FAISS + EMBEDDINGS) ---
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.schema import Document
